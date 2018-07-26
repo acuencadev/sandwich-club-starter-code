889bdc8326059cf879944f92fabf603220d72e4e
Amador Cuenca <sphi02ac@gmail.com>
2018-07-26 08:00:01 -0500

    feat: create Sandwich object from JSON array.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,53 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich;
+
+        try {
+            JSONObject object = new JSONObject(json);
+
+            String mainName = object.getJSONObject("name").getString("mainName");
+            List<String> alsoKnownAs = jsonArrayToList(object.getJSONObject("name").getJSONArray("alsoKnownAs"));
+
+            String placeOfOrigin = object.getString("placeOfOrigin");
+            String description = object.getString("description");
+            String image = object.getString("image");
+
+            List<String> ingredients = jsonArrayToList(object.getJSONArray("ingredients"));
+
+            sandwich = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image,
+                    ingredients);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return sandwich;
+    }
+
+    private static List<String> jsonArrayToList(JSONArray jsonArray) {
+        List<String> list = new ArrayList<>();
+
+        try {
+            for (int i = 0; i < jsonArray.length(); i++) {
+                list.add(jsonArray.getString(i));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return list;
     }
 }
